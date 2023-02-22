1b3282e41eced0e5fa6a187d9d6c514f18b79de8
lescano_lucas <lescano_lucas@hotmail.com>
2023-02-22 13:07:25 -0300

    agregados scripts

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,8 @@
-const API_URL = '';
-// const data = JSON.parse(this.response);
-const data = fetch(API_URL)
-console.log(data)
 
-let cardTitle = document.getElementsByClassName(card-title)
-cardTitle.innerHTML = `${events.name}`
+var cards = document.getElementById("cards");
+let events = data.events
+
+
+for (let index = 0; index < events.length; index++) {
+  cards.innerHTML += `<div class="card" style="width: 18rem;"><img src=${events[index].image} class="card-img-top cardimg" alt="..."><div class="card-body"><h5 class="card-title cardtitle">${events[index].name}</h5><p class="card-text cardtext">${events[index].description}</p><a href="#" class="btn btn-primary" id="btncard">Ver mas</a></div></div>`
+}
---
+++ scripts/upcoming_events.js
@@ -0,0 +1,11 @@
+const currentDate = data.currentDate
+let events = data.events
+var cards = document.getElementById("cards");
+
+for (let index = 0; index < events.length; index++) {
+
+  if (events[index].date >= currentDate) {
+    cards.innerHTML += `<div class="card" style="width: 18rem;"><img src=${events[index].image} class="card-img-top cardimg" alt="..."><div class="card-body"><h5 class="card-title cardtitle">${events[index].name}</h5><p class="card-text cardtext">${events[index].description}</p><a href="#" class="btn btn-primary" id="btncard">Ver mas</a></div></div>`
+  }
+
+}
