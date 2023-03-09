cca81a9d56c4007eb91732ec301186f356de53b0
lescano_lucas <lescano_lucas@hotmail.com>
2023-03-09 09:00:37 -0300

    agregados past event y esos

--- scripts/details.js
+++ scripts/details.js
@@ -1,5 +1,20 @@
-let container = document.getElementsByClassName('.container')
-let cards = document.getElementById("cards");
 let events = data.events;
-let search = document.getElementById('search');
-let checkboxes = document.querySelectorAll('input[type=checkbox]')
+let fragment = document.createDocumentFragment();
+
+const queryString = location.search
+
+const params = new URLSearchParams(queryString)
+
+const id = params.get('id')
+
+const cardId = events.find(e => e._id == id)
+
+const div = document.querySelector('.container')
+div.innerHTML = `<div class="card" style="width: 18rem;">
+<img src="${cardId.image}" class="card-img-top" alt="imagen de ${cardId.name}">
+<div class="card-body d-flex flex-column align-items-center">
+  <h5 class="card-title">${cardId.name}</h5>
+  <p class="card-text">${cardId.description}</p>
+  <a href="./home.html" class="btn btn-primary">Volver al inicio</a>
+</div>
+</div>`
--- scripts/upcoming_events.js
+++ scripts/upcoming_events.js
@@ -1,42 +1,123 @@
 const currentDate = data.currentDate
-let events = data.events
-var cards = document.getElementById("cards");
-let fragment = document.createDocumentFragment()
+let cards = document.getElementById("cardsList");
+let cardsDiv = cards.getElementsByTagName('div');
+let events = data.events;
+let fragment = document.createDocumentFragment();
+let card = cards.getElementsByClassName('card')
 
-for (evento of events) {
-  if (evento.date >= currentDate) {
-    let content = document.createElement('div')
-    content.className = 'card'
 
-    let img = document.createElement('img')
-    img.src = evento.image
-    img.className = 'card-img-top cardimg'
-    img.alt = 'cartas eventos'
-    content.appendChild(img)
 
-    let cardBody = document.createElement('div')
-    cardBody.className = 'card-body'
-    content.appendChild(cardBody)
+function crearCards() {
+  cardsList.innerHTML = ''
+  for (element of events) {
+    if (element.date >= currentDate) {
+      let content = document.createElement('div')
+      content.id = element._id
+      content.className = 'card'
 
-    let cardTitle = document.createElement('h5')
-    cardTitle.className = 'card-title cardtitle'
-    cardTitle.textContent = evento.name
-    cardBody.appendChild(cardTitle)
+      let img = document.createElement('img')
+      img.src = element.image
+      img.className = 'card-img-top cardimg'
+      img.alt = 'cartas elements'
+      content.appendChild(img)
 
-    let cardText = document.createElement('p')
-    cardText.className = 'card-text cardtext'
-    cardText.textContent = evento.description
-    cardBody.appendChild(cardText)
+      let cardBody = document.createElement('div')
+      cardBody.className = 'card-body'
+      content.appendChild(cardBody)
 
-    let btn = document.createElement('a')
-    btn.href = '#'
-    btn.className = 'btn btn-primary'
-    btn.id = 'btncard'
-    btn.textContent = 'Ver mas'
-    cardBody.appendChild(btn)
+      let cardTitle = document.createElement('h5')
+      cardTitle.className = 'card-title cardtitle'
+      cardTitle.textContent = element.name
+      cardBody.appendChild(cardTitle)
 
-    fragment.appendChild(content)
+      let cardText = document.createElement('p')
+      cardText.className = 'card-text cardtext'
+      cardText.textContent = element.description
+      cardBody.appendChild(cardText)
+
+      let btn = document.createElement('a')
+      btn.href = `./details.html?id=${element._id}`
+      btn.className = 'btn btn-primary'
+      btn.id = 'btncard'
+      btn.textContent = 'Ver mas'
+      cardBody.appendChild(btn)
+
+      let cardCategory = document.createElement('span')
+      cardCategory.textContent = element.category
+      cardCategory.className = element.category
+      cardCategory.style.display = 'none'
+      content.appendChild(cardCategory)
+
+      fragment.appendChild(content)
+    }
+  }
+  cards.appendChild(fragment);
+}
+
+crearCards()
+
+cards.appendChild(fragment)
+
+function filterCards() {
+  // Obtiene el valor del input de búsqueda
+  var searchValue = document.getElementById("searchInput").value.toLowerCase();
+
+
+  // Recorre todas las cartas y oculta las que no coinciden con la búsqueda
+  for (var i = 0; i < cardsDiv.length; i++) {
+    var cardText = cardsDiv[i].innerHTML.toLowerCase();
+    if (cardText.indexOf(searchValue) > -1) {
+      cardsDiv[i].style.display = "";
+    } else {
+      cardsDiv[i].style.display = "none";
+    }
   }
 }
 
-cards.appendChild(fragment)
+var searchInput = document.getElementById("searchInput");
+searchInput.addEventListener("input", filterCards);
+
+const checkboxContainer = document.getElementById("checkboxContainer");
+let categorias = []
+events.forEach(e => {
+  if (!categorias.includes(e.category)) {
+    categorias.push(e.category)
+  }
+})
+
+categorias.forEach((categoria) => {
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.value = categoria;
+  checkbox.textContent = categoria;
+  checkboxContainer.appendChild(checkbox);
+
+  let div = document.createElement('div')
+  div.textContent = categoria
+  checkboxContainer.appendChild(div)
+});
+
+
+function actualizarCartas() {
+  const categoriasSeleccionadas = Array.from(checkboxContainer.querySelectorAll("input:checked")).map((checkbox) => checkbox.value);
+  for (let i = 0; i < card.length; i++) {
+    if (categoriasSeleccionadas.length == 0) {
+      crearCards()
+    }
+    if (card[i].style.display == "none" && card[i].style.display == 0) {
+      console.log('hola');
+    }
+    if (categoriasSeleccionadas.includes(events[i].category)) {
+      card[i].style.display = "block";
+    } else {
+      card[i].style.display = "none";
+    }
+  }
+
+
+}
+
+
+checkboxContainer.querySelectorAll("input").forEach((checkbox) => {
+  checkbox.addEventListener("click", actualizarCartas);
+});
